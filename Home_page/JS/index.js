4e16879b8c689fbf4e4d8eb58829960769e34aa0
Tharindu209 <tharindudamruwan23@gmail.com>
2023-01-03 22:08:36 +0530

    final commit
    add homepage contend and modify local paths

---
+++ Home_page/JS/index.js
@@ -0,0 +1,42 @@
+const btnBars = document.querySelector('.bars');
+const btnTimes = document.querySelector('.times');
+const SideNav = document.querySelector('.aside');
+
+
+btnBars.addEventListener('click', () => myFunc('open'));
+btnTimes.addEventListener('click', () => myFunc('close'));
+
+const myFunc = (navCondition) => {
+    if(navCondition === 'open'){
+        SideNav.classList.add('show-nav');
+        btnTimes.style.display = "block";
+        btnBars.style.display = "none";
+    }
+    else if(navCondition === 'close'){
+            SideNav.classList.remove('show-nav');
+            btnTimes.style.display = "none";
+            btnBars.style.display = "block";
+    }
+}
+
+
+// canvas
+$(document).ready(function (){
+    if(!$("#myCanvas").tagcanvas({
+        textColour: "#08fdd8",
+        outlineColour: "transparent",
+        reverse: true,
+        depth: 1,
+        maxSpeed: 0.05,
+        weight: true,
+    }, "tags")){
+        // something went wrong hide the canvas container,
+        $("#myCanvasContainer");
+    }
+})
+
+// preloader
+window.addEventListener('load',() =>{
+    const preloader = document.querySelector(".preloader"); 
+    preloader.classList.add("preloader-finish");
+});
